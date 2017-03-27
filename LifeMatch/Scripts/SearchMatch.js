98e4e515ebac1c410c945d86466777d31645c18e
amir.kublansky <amir.kublansky@toluna.com>
2017-03-27 11:00:30 +0300

    added solution with client server basic call

---
+++ LifeMatch/Scripts/SearchMatch.js
@@ -0,0 +1,18 @@
+﻿angular.module('SearchMatch', [])
+        .controller('SearchMatchController', function ($scope, $http) {
+
+            $scope.ButtonTitle = {
+                Text: "Click to call the server"
+            }
+
+            $scope.showMessage = false;
+
+            $scope.callServer = function () {
+                $http.get("HelloFromServer")
+                .then(function (member) {
+                    $scope.FirstName = member.data.FirstName;
+                    $scope.LastName = member.data.LastName;
+                    $scope.showMessage = true;
+                    });
+            };
+        });
