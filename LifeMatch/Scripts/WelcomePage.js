b274e400ec9c09b6b7596cd8eadf150aefea1210
amir.kublansky <amir.kublansky@toluna.com>
2017-03-27 19:10:05 +0300

    Cleaned Index to have a pure angular page and added redirect to lifeMatch page

---
+++ LifeMatch/Scripts/WelcomePage.js
@@ -0,0 +1,10 @@
+﻿angular.module('WelcomePage', [])
+        .controller('WelcomePageController', function ($scope, $window, $http) {
+
+            $scope.test = "Angular test succeeded";
+
+            $scope.redirectToSearchMatch = function () {
+                var url = "http://" + $window.location.host + "/Home/SearchMatch";
+                $window.location.href = url;
+            };
+        });
