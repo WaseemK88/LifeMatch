c1927f078875cd05427fd70a18de997b2c512be9
WaseemKhatib <waseem.khatib@toluna.com>
2017-03-29 17:40:18 +0300

    Adding DB connector and user models

---
+++ LifeMatch/DBClient/UserInfoManagement.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using LifeMatch.LifeMatchModels;
+
+namespace LifeMatch.DBClient
+{
+    // user module
+    public class UserInfoManagement
+    {
+        public UserInfoManagement() { }
+
+        public UserModel GetUserById(int userId)
+        {
+            // run get query
+            return null;
+        }
+
+        public void InsertNewUser(UserModel newUser)
+        {
+            // run insert query
+        }
+
+        public void UpdateUserById(UserModel updatedUserData)
+        {
+            // run update query
+        }
+    }
+}
