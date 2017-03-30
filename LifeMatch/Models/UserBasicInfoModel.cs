bd203414881f35388d2d4b48fbe1802285cd138d
WaseemKhatib <waseem.khatib@toluna.com>
2017-03-30 21:51:31 +0300

    DB connector + basic user info model

---
+++ LifeMatch/Models/UserBasicInfoModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LifeMatch.Models
+{
+    public class UserBasicInfoModel
+    {
+        // user info scheme
+        public int UserId;
+        public Enums.Gender Gender;
+        public int Age;
+        public string Name;
+        public string LastName;
+        public string NickName;
+        public string FacebookId;
+
+        public UserBasicInfoModel()
+        {
+        }
+
+        public UserBasicInfoModel(UserBasicInfoModel user)
+        {
+            UserId = user.UserId;
+            Gender = user.Gender;
+            Age = user.Age;
+            Name = user.Name;
+            LastName = user.LastName;
+            NickName = user.NickName;
+            FacebookId = user.FacebookId;
+        }
+    }
+}
